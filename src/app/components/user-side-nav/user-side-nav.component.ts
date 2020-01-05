dab85fd0f60326a8328574d6a433db37f92c22c8
AlexanderCarlston <alekscarlston@gmail.com>
2020-01-05 13:43:27 -0700

    Update side nav

--- src/app/components/user-side-nav/user-side-nav.component.ts
+++ src/app/components/user-side-nav/user-side-nav.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ApiService, User } from 'src/app/services/api.service';
 
 @Component({
-  selector: 'user-side-nav',
+  selector: 'app-user-side-nav',
   templateUrl: './user-side-nav.component.html',
   styleUrls: ['./user-side-nav.component.scss']
 })
@@ -15,7 +15,7 @@ export class UserSideNavComponent implements OnInit {
     this.apiService.getUser(this.user.user_id)
     .subscribe(value => {
       this.userData = value.data[0];
-      console.log(this.userData)
+      console.log(this.userData);
     });
   }
 
